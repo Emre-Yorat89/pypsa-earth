8d676f97119aa30049b36a0b74969a0a069b0232
Emre_Yorat <62134151+Emre-Yorat89@users.noreply.github.com>
2025-11-14 13:51:29 +0300

    Update build_shapes.py

--- scripts/build_shapes.py
+++ scripts/build_shapes.py
@@ -1542,8 +1542,7 @@ if __name__ == "__main__":
         subregion_shapes = pd.DataFrame()
 
     save_to_geojson(subregion_shapes, out.subregion_shapes)
-
-    if config["enable"]["run_distribution"] == True:
+    if snakemake.params.pypsa_distribution_shape["run_distribution"]:
         create_microgrid_shapes(
         snakemake.config["microgrids_list"],
         snakemake.output["microgrid_shapes"],
